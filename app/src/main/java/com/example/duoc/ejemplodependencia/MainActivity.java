0cddaf5a6c7f3787f4d046c27c1553e9e7cf6f68
pablo jerez <pablojerezy@gmail.com>
2016-11-03 14:40:37 -0300

    commit inicial

--- app/src/main/java/com/example/duoc/ejemplodependencia/MainActivity.java
+++ app/src/main/java/com/example/duoc/ejemplodependencia/MainActivity.java
@@ -1,37 +1,26 @@
 package com.example.duoc.ejemplodependencia;
 
-import android.support.v7.app.AppCompatActivity;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private MapView myOpenMapView;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        myOpenMapView = (MapView)findViewById(R.id.openmapview);
+        myOpenMapView.setBuiltInZoomControls(true);
+        IMapController controller = myOpenMapView.getController();
+        controller.setZoom(4);
+        myOpenMapView.setMultiTouchControls(true);
     }
 }
